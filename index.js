2ee94302b80a4df9c397ca84d88be8f9abf8844b
ardiiiv <ardiiiv222@gmail.com>
2025-12-12 00:41:28 +0700

    membuat file di routes dan install dotenv

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
-const host = "localhost";
 
+// middleware start
+app.use(express.json())
+// middleware end
+
+// router start
 app.use("/", (req, res) => {
     res.send("<h1>Hello World!!</h1>")
 });
+// router end
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
 
 app.listen(port, host, () => {
     console.log(`Example app listening on port http://${host}:${port}`);
